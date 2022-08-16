fa71583bd6fc6ca34f0f2b1035974db445ec7055
Boris Pouw <74407773+BPouw@users.noreply.github.com>
2022-08-16 15:59:57 +0200

    ng material

--- src/app/components/matches/matches.component.ts
+++ src/app/components/matches/matches.component.ts
@@ -1,47 +1,29 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Data, Router} from '@angular/router'
-import { DataTableDirective, DataTablesModule} from 'angular-datatables'
-import { Observable, Subject } from 'rxjs';
 import { Match } from 'src/app/models/match.model';
 import { MatchService} from 'src/app/services/match.service'
-import { TeamService } from 'src/app/services/team.service'
 
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
   styleUrls: ['./matches.component.css']
 })
+
 export class MatchesComponent implements OnInit {
 
-  @ViewChild(DataTableDirective, {static: false})
-  dtElement!: DataTableDirective
-  dtOptions: DataTables.Settings = {}
-  dtTrigger: Subject<any> = new Subject<any>()
-
-  matches$!: Observable<Match[]>
   matches: Match[] = []
+  columnsToDisplay = ['date', 'matchname', 'map', 'teams', 'score', 'winner']
   selectedId = 0
 
   batchSize = 10
   batchNmbr = 0
 
-  constructor(private matchService: MatchService, private teamService: TeamService) { }
+  constructor(private matchService: MatchService) { }
 
   ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: "full_numbers",
-      pageLength: 10
-    }
-
     this.matchService.batch(this.batchSize, this.batchNmbr).subscribe(data => {
       this.matches = this.matches.concat(data)
-      console.log(data)
-
-      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-        dtInstance.destroy()
-        this.dtTrigger.next(this.matches)
-      })
-        
+      console.log(data)      
     })
 
 
@@ -51,15 +33,12 @@ export class MatchesComponent implements OnInit {
     this.batchNmbr += 1
     this.matchService.batch(this.batchSize, this.batchNmbr).subscribe(data => {
       this.matches = this.matches.concat(data)
-      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-        dtInstance.destroy()
-        this.dtTrigger.next(this.matches)
-      })
+
     })
   }
 
   ngOnDestroy(): void {
-    this.dtTrigger.unsubscribe();
+    
   }
 
 }
