bbf10ffe36ef3b6d02e4352e2a8d95aaef407ef8
Boris Pouw <74407773+BPouw@users.noreply.github.com>
2022-08-16 12:30:06 +0200

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule} from '@angular/material/icon';
+import { MatchesComponent } from './components/matches/matches.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MatchesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
