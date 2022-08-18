4b4db63544cff4b5d1cc44f47b3db4066c751371
Boris Pouw <74407773+BPouw@users.noreply.github.com>
2022-08-18 15:26:34 +0200

    playerdetails = done

--- src/app/components/player-details/player-details.component.ts
+++ src/app/components/player-details/player-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Player } from 'src/app/models/player.model';
+import { Team } from 'src/app/models/team.model';
+import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
   selector: 'app-player-details',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerDetailsComponent implements OnInit {
 
-  constructor() { }
+  player!: Player;
+  team!: Team;
+  agents: String[] = []
+  lowAgents: String[] = []
+
+  constructor(private activatedRoute: ActivatedRoute, private playerService: PlayerService, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.playerService.read(params['id']).subscribe(data => {
+        this.player = data;
+        this.team = this.player.team;
+        this.agents = this.player.agents
+        for(let agent of this.agents) {
+          let lowagent = agent.toLowerCase()
+          this.lowAgents.push(lowagent)
+        }
+        console.log(this.lowAgents)
+      })
+    })
+  }
+
+  teamDetails(team: Team): void {
+    this.router.navigate(["teams", team._id])
   }
 
 }
