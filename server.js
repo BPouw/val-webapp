35e928fb3daea7384bcf236431aeee2d0c0bca45
Boris Pouw <74407773+BPouw@users.noreply.github.com>
2022-08-19 14:00:38 +0200

    :)

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+const options = {
+  setHeaders: (res, path, stat) => {
+    res.set(
+      'Content-Security-Policy',
+      "default-src 'self' http://angular-gitlab-heroku.herokuapp.com/; script-src 'self' http://angular-gitlab-heroku.herokuapp.com/; connect-src http://angular-gitlab-heroku.herokuapp.com/ 'self'; img-src 'self' www.google.com; style-src 'self' 'unsafe-inline';"
+    )
+  }
+}
+
+
+app.use(express.static(__dirname + '/dist'), options);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+
 
 app.listen(process.env.PORT || 4200);
