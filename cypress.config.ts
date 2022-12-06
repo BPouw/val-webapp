1937fd1802aeb782c7838de21387d5ef1433559d
boris.pouw <bj.pouw@student.avans.nl>
2022-12-06 21:01:49 +0100

    im thinking last pipeline run

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    projectId: "zyyqf5",
-    env: {
-      api: 'https://val-api.herokuapp.com/api/'
-    }
+    projectId: "zyyqf5"
   }
 })
